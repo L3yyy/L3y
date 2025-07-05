b6da6df426719e20bac30213e510f2d69a852b50
L3Y <kuffiwrld999@gmail.com>
2025-07-05 23:01:27 +0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const instructions = document.createElement("span");
+instructions.innerHTML = "‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ <strong>Type 'help' for a list of commands.</strong><br>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ <strong>Type 'leyfetch' to fetch my social media profiles.</strong><br>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ <strong>Type 'projects' to see my projects.</strong><br><br>";
+instructions.setAttribute("style", "color: #EBDBB2; font-family: monospace;");
+document.body.appendChild(instructions);
+
 function user() {
+//Type 'help' for a list of commands.
+//Type 'leyfetch' to fetch my social media profiles.
+//Type 'projects' to see my projects.
 
     const my_span = document.createElement("span");
     my_span.innerHTML = "user";
@@ -64,38 +72,37 @@ function cmd(){
     const cons_val = document.createElement("cons_val");
     cons_value(my_user_input.value);
 
-    if (my_user_input.value == "clear") {
+    if (my_user_input.value == "clear" || my_user_input.value == "-cl") {
 
         document.body.innerHTML = "";
         user();
         
         return;
 
-    }   else if (my_user_input.value == "help") {
+    }   else if (my_user_input.value == "help" || my_user_input.value == "-h") {
         br();
 
         //clear, help, leyfetch, projects
         cons_valuebr(" ");
-        cons_valuebr("welcome to my portfolio!");
-        cons_valuebr("----------------------------------");
-        cons_valuebr("these are the commands you can use");
-        cons_valuebr("----------------------------------");
-        cons_valuebr("help                 lists all the commands you can use.");
-        cons_valuebr("leyfetch             fetches my social media profiles.");
-        cons_valuebr("clear                clears the terminal.");
-        cons_valuebr("projects             shows you the projects i have worked on.");
-        cons_valuebr("restart              refreshes the website for a new start.");
+        cons_valuebr("welcome to my portfolio! here are some commands you can use:");
+        cons_valuebr("------------------------------------------------------------");
+        cons_valuebr("help,     -h              lists all the available commands.");
+        cons_valuebr("leyfetch, -lf             fetches my social media profiles.");
+        cons_valuebr("clear,    -cl             clears the terminal.");
+        cons_valuebr("projects, -pr             shows the projects i've worked on.");
+        cons_valuebr("restart,  -rt             refreshes the website for a new start.");
+        cons_valuebr("about,    -ab             shows information behind the developers of <br>                          <strong>L3Y Terminal</strong>.");
 
         my_user_input.value = "";
         br();
 
         
-    }   else if (my_user_input.value == "leyfetch") {
+    }   else if (my_user_input.value == "leyfetch" || my_user_input.value == "-lf") {
 
         br();
         cons_valuebr(" ");
         cons_valuebr("    <label style='color:#EBDBB2'>/\\_____/\\        email</label>: kuffiwrld999<label style='color:#EBDBB2'>@</label>gmail.com");
-        cons_valuebr("   <label style='color:#EBDBB2'>/</label>  <label style='color:white'>o   o</label>  <label style='color:#EBDBB2'>\\</label>       <label style='color:#EBDBB2'>-----------");
+        cons_valuebr("   <label style='color:#EBDBB2'>/</label>  <label style='color:white'>o   o</label>  <label style='color:#EBDBB2'>\\</label>       <label style='color:#EBDBB2'>--------------------------------------------------");
         cons_valuebr("  <label style='color:#EBDBB2'>(</label> <label style='color:white'>==  ^  == </label><label style='color:#EBDBB2'>)</label>      <label style='color:#EBDBB2'>discord</label>: <a href='https://discord.gg/wV86RZUR7V'>https://discord.gg/wV86RZUR7V</a>");
         cons_valuebr("   <label style='color:#EBDBB2'>)         (       facebook</label>: <a href='https://www.facebook.com/LLUVSICC/'>https://www.facebook.com/LLUVSICC/</a>");
         cons_valuebr("  <label style='color:#EBDBB2'>(           )      youtube</label>: <a href='https://www.youtube.com/@prodbyluvsic'>https://www.youtube.com/@prodbyluvsic</a>");
@@ -105,13 +112,24 @@ function cmd(){
 
         my_user_input.value = "";
 
-    }   else if (my_user_input.value == "restart") {
-        location.reload();
-    } else if (my_user_input.value == "projects"){
+    }   else if (my_user_input.value == "restart" || my_user_input.value == "-rt") {
         br();
-        cons_value("redirecting..")
+        cons_valuebr("restarting...");
+        location.reload();
+
+    } else if (my_user_input.value == "projects" || my_user_input.value == "-pr") {
+        br();
+        cons_value("projects will be available soon!");
         window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXCgBwE%3D', '_blank');
         br();
+
+    } else if (my_user_input.value == "about" || my_user_input.value == "-a") {
+        br();
+        cons_valuebr(" ");
+        cons_valuebr("<label style='color:#8F8CF2'><h3>@L3yyy</h3></label><br>Hola! I'm L3yyy, a 17 year old dev-hobbyist from the Philippines.<br>I've loved computers since I was 4 years old. I started coding at the<br>age of 12 and have been learning ever since. My goal is to become<br>a full-stack developer and maybe take on some freelance work");
+        cons_valuebr("<label style='color:#8F8CF2'><h3>@Demobnector</h3></label><br>WIP");
+        cons_valuebr(" ");
+        my_user_input.value = "";
 
     } else {
         br();
@@ -130,7 +148,7 @@ function loop() {
     requestAnimationFrame(loop);
     const my_user_input = document.getElementById("user_input");
 
-    if (my_user_input.value == "help" || my_user_input.value == "projects" || my_user_input.value == "leyfetch" || my_user_input.value == "clear" || my_user_input.value == "" || my_user_input.value == "restart") {
+    if (my_user_input.value == "help" || my_user_input.value == "projects" || my_user_input.value == "leyfetch" || my_user_input.value == "clear" || my_user_input.value == "" || my_user_input.value == "restart" || my_user_input.value == "about" || my_user_input.value == "-h" || my_user_input.value == "-lf" || my_user_input.value == "-cl" || my_user_input.value == "-pr" || my_user_input.value == "-rt" || my_user_input.value == "-a") {
         my_user_input.setAttribute("style", "color: #EBDBB2; background: none; border: hidden; outline: none; font-family: monospace");
     
     } else {
