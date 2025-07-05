66ead0da310c6ecc36bc9d3ca30d74619c28d792
L3Y <kuffiwrld999@gmail.com>
2025-07-05 16:16:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,148 @@
+function user() {
+
+    const my_span = document.createElement("span");
+    my_span.innerHTML = "user";
+    my_span.setAttribute("style", "color:#EBDBB2");
+    my_span.setAttribute("class", "console");
+
+    document.body.appendChild(my_span);
+
+    const my_span1 = document.createElement("span");
+    my_span1.innerHTML = "@";
+    my_span1.setAttribute("style", "color:white");
+    my_span1.setAttribute("class", "console");
+
+    document.body.appendChild(my_span1);
+
+    const my_span2 = document.createElement("span");
+    my_span2.innerHTML = "test";
+    my_span2.setAttribute("style", "color:#EBDBB2");
+    my_span2.setAttribute("class", "console");
+
+    document.body.appendChild(my_span2);
+
+    const my_span3 = document.createElement("span");
+    my_span3.innerHTML = ":~ # ";
+    my_span3.setAttribute("style", "color:white");
+    my_span3.setAttribute("class", "console");
+
+    document.body.appendChild(my_span3);
+
+    const input = document.createElement("input");
+    input.setAttribute("style", "color: grey; background: none; border: hidden; outline: none; font-family: monospace");
+    input.setAttribute("id", "user_input");
+    input.setAttribute("type", "text");
+
+    document.body.appendChild(input);
+
+}
+
+function br() {
+    const linebreak = document.createElement("br");
+    document.body.appendChild(linebreak);
+}
+
+function cons_value(value) {
+    const cons_val = document.createElement("span");
+    cons_val.setAttribute("style", "color: grey; ");
+    cons_val.innerHTML = value;
+    document.body.appendChild(cons_val);
+}
+
+function cons_valuebr(value) {
+    const cons_val = document.createElement("span");
+    cons_val.setAttribute("style", "color: grey; white-space: pre;");
+    cons_val.innerHTML = value;
+    document.body.appendChild(cons_val)
+    br();
+}
+
+function cmd(){
+
+    const my_user_input = document.getElementById("user_input");
+    console.log(my_user_input.value);
+    const cons_val = document.createElement("cons_val");
+    cons_value(my_user_input.value);
+
+    if (my_user_input.value == "clear") {
+
+        document.body.innerHTML = "";
+        user();
+        
+        return;
+
+    }   else if (my_user_input.value == "help") {
+        br();
+
+        //clear, help, leyfetch, projects
+        cons_valuebr(" ");
+        cons_valuebr("welcome to my portfolio!");
+        cons_valuebr("----------------------------------");
+        cons_valuebr("these are the commands you can use");
+        cons_valuebr("----------------------------------");
+        cons_valuebr("help                 lists all the commands you can use.");
+        cons_valuebr("leyfetch             fetches my social media profiles.");
+        cons_valuebr("clear                clears the terminal.");
+        cons_valuebr("projects             shows you the projects i have worked on.");
+        cons_valuebr("restart              refreshes the website for a new start.");
+
+        my_user_input.value = "";
+        br();
+
+        
+    }   else if (my_user_input.value == "leyfetch") {
+
+        br();
+        cons_valuebr(" ");
+        cons_valuebr("    <label style='color:#EBDBB2'>/\\_____/\\        email</label>: kuffiwrld999<label style='color:#EBDBB2'>@</label>gmail.com");
+        cons_valuebr("   <label style='color:#EBDBB2'>/</label>  <label style='color:white'>o   o</label>  <label style='color:#EBDBB2'>\\</label>       <label style='color:#EBDBB2'>-----------");
+        cons_valuebr("  <label style='color:#EBDBB2'>(</label> <label style='color:white'>==  ^  == </label><label style='color:#EBDBB2'>)</label>      <label style='color:#EBDBB2'>discord</label>: <a href='https://discord.gg/wV86RZUR7V'>https://discord.gg/wV86RZUR7V</a>");
+        cons_valuebr("   <label style='color:#EBDBB2'>)         (       facebook</label>: <a href='https://www.facebook.com/LLUVSICC/'>https://www.facebook.com/LLUVSICC/</a>");
+        cons_valuebr("  <label style='color:#EBDBB2'>(           )      youtube</label>: <a href='https://www.youtube.com/@prodbyluvsic'>https://www.youtube.com/@prodbyluvsic</a>");
+        cons_valuebr(" <label style='color:#EBDBB2'>( (  )   (  ) )     onlyfans</label>: <a href='https://only-fans.me/ley'>https://only-fans.me/ley</a>");
+        cons_valuebr("<label style='color:#EBDBB2'>(__(__)___(__)__)    speedrun</label>: <a href='https://blogs.mtdv.me/watch?v=UjbImfg7gn'>https://blogs.mtdv.me/watch?v=UjbImfg7gn</a>");
+        cons_valuebr(" ");
+
+        my_user_input.value = "";
+
+    }   else if (my_user_input.value == "restart") {
+        location.reload();
+    } else if (my_user_input.value == "projects"){
+        br();
+        cons_value("redirecting..")
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXCgBwE%3D', '_blank');
+        br();
+
+    } else {
+        br();
+        cons_valuebr("command not found");
+    } 
+
+
+    my_user_input.remove()
+    user();
+
+}
+
+user();
+
+function loop() {
+    requestAnimationFrame(loop);
+    const my_user_input = document.getElementById("user_input");
+
+    if (my_user_input.value == "help" || my_user_input.value == "projects" || my_user_input.value == "leyfetch" || my_user_input.value == "clear" || my_user_input.value == "" || my_user_input.value == "restart") {
+        my_user_input.setAttribute("style", "color: #EBDBB2; background: none; border: hidden; outline: none; font-family: monospace");
+    
+    } else {
+        my_user_input.setAttribute("style", "color: red; background: none; border: hidden; outline: none; font-family: monospace");
+    }
+  }
+  loop();
+
+document.addEventListener("keypress", function(e) {
+
+    if (e.key == "Enter"){
+        cmd();
+    }
+
+  });
